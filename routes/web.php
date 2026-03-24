d835a7b248d32a38e495956f0fbf675d8f3e7ebd
Kristian Palovic <kpskola1002@gmail.com>
2026-03-24 11:13:16 +0100

    feat: replace welcome view with dynamic Entry retrieval

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Statamic\Facades\Entry;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Entry::findOrFail('31fb0e30-69f5-44ce-9694-5ba2078e23e9');
 });
