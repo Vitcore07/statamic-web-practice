37e4e5224e701ad760023653b81d0275f60c54e1
Kristian Palovic <kpskola1002@gmail.com>
2026-03-24 11:39:50 +0100

    feat: add language-specific routes for Slovak and English

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,17 @@ use Illuminate\Support\Facades\Route;
 use Statamic\Facades\Entry;
 
 Route::get('/', function () {
-    return Entry::findOrFail('31fb0e30-69f5-44ce-9694-5ba2078e23e9');
+    $landingPage = Entry::findOrFail('31fb0e30-69f5-44ce-9694-5ba2078e23e9')->in('slovak');
+
+    abort_if(! $landingPage, 404);
+
+    return $landingPage;
+});
+
+Route::get('/en', function () {
+    $landingPage = Entry::findOrFail('31fb0e30-69f5-44ce-9694-5ba2078e23e9')->in('default');
+
+    abort_if(! $landingPage, 404);
+
+    return $landingPage;
 });
