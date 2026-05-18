9f89e1456dfd631f067f193073e5f0da94e507bf
Jancikos <ko.janci@gmail.com>
2026-05-18 16:19:24 +0200

    docker: maintenance scripts fix

--- docker/scripts/docker_compose_rebuild.sh
+++ docker/scripts/docker_compose_rebuild.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -e
 
+DOCKER_COMPOSE_FILE="compose.production.yaml"
+
+# if the file doesn't exist
+if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
+   # try ty check if it is not 2 levels up
+    if [ ! -f "../../$DOCKER_COMPOSE_FILE" ]; then
+        echo "Docker compose file not found!"
+        exit 1
+    else
+        DOCKER_COMPOSE_FILE="../../$DOCKER_COMPOSE_FILE"
+    fi
+fi
+
 # Rebuild necessary containers
-docker compose -f compose.production.yaml build server queue scheduler
+docker compose -f "$DOCKER_COMPOSE_FILE" build server queue scheduler
 
 # Ask if the user wants to restart the containers
 read -p "Do you want to restart the containers? (y/n) " -n 1 -r
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Restarting containers..."
-    docker compose -f compose.production.yaml up -d
+    docker compose -f "$DOCKER_COMPOSE_FILE" up -d
 else
     echo "Restart cancelled."
 fi
--- docker/scripts/docker_compose_up.sh
+++ docker/scripts/docker_compose_up.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
 
-docker compose -f compose.production.yaml up -d
+DOCKER_COMPOSE_FILE="compose.production.yaml"
+
+# if the file doesn't exist
+if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
+   # try ty check if it is not 2 levels up
+    if [ ! -f "../../$DOCKER_COMPOSE_FILE" ]; then
+        echo "Docker compose file not found!"
+        exit 1
+    else
+        DOCKER_COMPOSE_FILE="../../$DOCKER_COMPOSE_FILE"
+    fi
+fi
+
+docker compose -f "$DOCKER_COMPOSE_FILE" up -d
