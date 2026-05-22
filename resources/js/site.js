d81e32765c359946177bd3c53ba0e59f2eee0519
Vitcore07 <vitkorsin1@gmail.com>
2026-05-22 11:56:59 +0200

    tailwind upravy, js zaklad

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const prevBtn = document.getElementById('prev-btn');
-    const nextBtn = document.getElementById('next-btn');
+    const prevBtn = document.getElementById('prev-Btn');
+    const nextBtn = document.getElementById('next-Btn');
     const items = document.querySelectorAll('.slider-card');
 
     let currentIndex = 0;
 
     function slide(index) {
-        if (index < 0) {
-            index = items.length - 1;
-        } else if (index >= items.length) {
-            index = 0;
-        }
-        
-        currentIndex = index;
+    if (index < 0) {
+        index = items.length - 1;
+    } else if (index >= items.length) {
+        index = 0;
     }
+
+    currentIndex = index;
+}
 
     prevBtn.addEventListener('click', () => slide(currentIndex - 1));
     nextBtn.addEventListener('click', () => slide(currentIndex + 1));
     
     slide(0);
+    
 });
